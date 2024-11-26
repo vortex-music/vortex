0a91f64658fd950a046906b5cff69cf894ed32d1
Atheesh Thirumalairajan <github-commits@atheesh.org>
2024-11-25 22:49:03 -0500

    Conflict Resolution

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,12 @@ import TextField from './components/base/TextField'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className='w-full h-full flex flex-col'>
-      <AppTitleBar /><hr />
-      <Drawer>
-        <TextField />
-      </Drawer>
-=======
     <div className='w-full h-full flex flex-col bg-app_background'>
       <AppTitleBar />
       <hr className='border border-primary_border'/>
-      <Drawer>Sidebar!</Drawer>
->>>>>>> 63be5258b12abed3cc80122b77aaf25674f044cc
+      <Drawer>
+        <TextField />
+      </Drawer>
     </div>
   )
 }
